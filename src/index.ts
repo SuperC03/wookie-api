cea8a61152ebddee333e0802a283a2f5224ea4a9
DESKTOP-UH3M29M\clark <clarkbros3@gmail.com>
2019-08-11 18:51:28 +0200

    Setup Package

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+const rp = require('request-promise');
+
+exports.translate = (english: string): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    const options = {
+      body: {
+        english: 'hey',
+      },
+      json: true,
+      method: 'POST',
+      uri: 'https://www.wookietranslator.com/translate',
+    };
+    rp(options)
+      .then((res: any) => {
+        resolve(res as string);
+      })
+      .catch((err: any) => {
+        reject(err as string);
+      });
+  });
+};
